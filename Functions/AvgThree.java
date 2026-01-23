dae3967ad503a5032cb3bf1524d9ef31a4e9d28e
Divya Thakur <thakur.14divya@gmail.com>
2026-01-23 18:33:00 +0530

    Avg of three nos

---
+++ Functions/AvgThree.java
@@ -0,0 +1,10 @@
+class AvgThree
+{
+    public static int sum(int a,int b,int c){
+        int avg = (a+b+c)/2;
+        return avg;
+    }
+    public static void main(String args[]){
+        System.out.println(sum(23,56,80));
+    }
+}
