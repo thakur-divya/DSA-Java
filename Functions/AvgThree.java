6108c95e2d8695bf49dd4112899c30a6e45fc4b5
Divya Thakur <thakur.14divya@gmail.com>
2026-01-23 18:41:23 +0530

    changed avg from int to double

--- Functions/AvgThree.java
+++ Functions/AvgThree.java
@@ -1,10 +1,10 @@
 class AvgThree
 {
-    public static int sum(int a,int b,int c){
-        int avg = (a+b+c)/2;
+    public static double sum(double a,double b,double c){
+        double avg = (a+b+c)/3;
         return avg;
     }
     public static void main(String args[]){
-        System.out.println(sum(23,56,80));
+        System.out.println(sum(23.25,56.32,80.12));
     }
 }
