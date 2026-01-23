6a0d383e9ebcec41d64af34d57c55b9e556b6db7
Divya Thakur <thakur.14divya@gmail.com>
2026-01-23 18:47:57 +0530

    Checking if no is even

---
+++ Functions/isEven.java
@@ -0,0 +1,16 @@
+package Functions;
+
+public class isEven {
+    public static boolean isEven(int n){
+        if(n%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+    public static void main(String args[]){
+        System.out.println(isEven(8));
+    }
+}
