be33c5e72842f14c7b29650524d5f5df59e3cef5
Divya Thakur <thakur.14divya@gmail.com>
2026-01-26 22:54:43 +0530

    Floyds Triangle

---
+++ Pattern/FloydsTriangle.java
@@ -0,0 +1,17 @@
+package Pattern;
+
+public class FloydsTriangle {
+    public static void Floyds(int n){
+        int k=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(k + " ");
+                k++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Floyds(5);
+    }
+}
