4e47f3e03cbb487c02bf5fb73e2ea6eee99accc7
Divya Thakur <thakur.14divya@gmail.com>
2026-01-29 23:19:05 +0530

    Taking input in array

---
+++ Arrays/Arrayinput.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class Arrayinput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int marks[] = new int[100];
+
+         marks[0]=sc.nextInt();//phy
+         marks[1]=sc.nextInt();//chem
+         marks[2]=sc.nextInt();//maths
+
+         System.out.println("phy : " + marks[0]);
+         System.out.println("chem : " + marks[1]);
+         System.out.println("maths : " + marks[2]);
+
+    }
+}
