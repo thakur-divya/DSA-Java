c92b616d4147ab0e1ce51ad23c8f2965f5e20c6a
Divya Thakur <thakur.14divya@gmail.com>
2026-01-30 02:04:46 +0530

    Pairs in array

--- Arrays/Pairs.java
+++ Arrays/Pairs.java
@@ -1,15 +1,19 @@
 package Arrays;
 
 public class Pairs {
+    //Time complexity is O(n2)
+
     public static void printpairs(int numbers[]){
+        int tp=0;
         for(int i=0;i<=numbers.length-1;i++){
             int curr = numbers[i]; //2,4,6,8
             for(int j=i+1;j<=numbers.length-1;j++){
                 System.out.print("(" + curr + "," + numbers[j] + ")");
+                tp++;
             }
             System.out.println();
         }
-
+        System.out.println("Total Pairs in Array : " + tp);
     }
     public static void main(String[] args) {
         int numbers[]={2,4,6,8,10};
