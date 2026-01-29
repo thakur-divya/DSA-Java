87b70873e038cfff364dda22450ba41f742dbed5
Divya Thakur <thakur.14divya@gmail.com>
2026-01-29 23:57:09 +0530

    Largest no in given array

---
+++ Arrays/LargestArray.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class LargestArray {
+    public static int getlargest(int numbers[]){
+        int largest= Integer.MIN_VALUE;
+        for(int i=0;i<numbers.length;i++){
+            if(numbers[i]>largest){
+                largest=numbers[i];
+            }
+        }
+        return largest;
+    }   
+    public static void main(String[] args) {
+        int numbers[]={23,78,34,89,20,1,54,73,42};
+        int largest = getlargest(numbers);
+        System.out.println("Largest no in array is : " + largest);
+    } 
+}
