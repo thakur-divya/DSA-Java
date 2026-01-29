97d151361268d0e0d3df2cf3bda0dd15e19df7a6
Divya Thakur <thakur.14divya@gmail.com>
2026-01-30 01:22:02 +0530

    Binary search in array

---
+++ Arrays/Binarysearch.java
@@ -0,0 +1,25 @@
+package Arrays;
+public class Binarysearch {
+    public static int binarysearch(int numbers[],int key){
+        int start=0;
+        int end=numbers.length+1;
+        while(start<=end){
+            int mid=(start + end)/2;
+            if(numbers[mid] == key){
+                return mid;
+            }
+            if(numbers[mid] < key){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int numbers[]={10,20,30,45,67,89,90};
+        int key=45;
+        System.out.println("Element found at index : " + binarysearch(numbers, key));
+    }
+}
