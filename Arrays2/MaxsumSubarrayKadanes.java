cf612dfa6b1157edfa80f5af89794060710648fc
Divya Thakur <thakur.14divya@gmail.com>
2026-02-01 00:05:45 +0530

    Kadanes algo to solve max sum subarray

---
+++ Arrays2/MaxsumSubarrayKadanes.java
@@ -0,0 +1,25 @@
+package Arrays2;
+
+public class MaxsumSubarrayKadanes {
+    public static void maxsum(int number[]){
+        int cursum =0;
+        int maxsum = Integer.MIN_VALUE;
+        for(int i=0;i<number.length;i++){
+            cursum = cursum + number[i];
+            
+            if(cursum < 0){
+                cursum = 0;
+            }
+            if(cursum > maxsum){
+                maxsum = cursum;
+            }
+            System.out.println("CS : " + cursum + " " + "MS : " + maxsum + " ");
+            
+        }
+        System.out.println("Maximum sum is : " + maxsum);
+    }
+    public static void main(String[] args) {
+        int numbers[]={-2,-3,4,-1,-2,1,5,-3};
+        maxsum(numbers);
+    }
+}
