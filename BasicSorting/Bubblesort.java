45c4d77ba93c42c0e3e6d6d2cee122edfb60ff4a
Divya Thakur <thakur.14divya@gmail.com>
2026-02-03 00:18:45 +0530

    Bubble sorting

--- BasicSorting/Bubblesort.java
+++ BasicSorting/Bubblesort.java
@@ -2,12 +2,12 @@ package BasicSorting;
 
 public class Bubblesort {
     public static void bubble(int arr[]){
-        for(int i=0; i<arr.length; i++){
-            for(int j=i+1; j<arr.length ; j++){
-                if(arr[j]>arr[i]){
-                    int temp = arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=i+1; j<arr.length-1-i ; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
                 }
             }
         }
