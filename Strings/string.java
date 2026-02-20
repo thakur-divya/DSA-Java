1114d65e39d498f03e695fa0c2c11730ffc223af
Divya Thakur <thakur.14divya@gmail.com>
2026-02-20 12:33:31 +0530

    basics

---
+++ Strings/string.java
@@ -0,0 +1,17 @@
+
+package Strings;
+import java.util.*;
+
+//Strings are IMMUTABLE in Java
+public class string {
+   public static void main(String[] args) {
+        char arr[] = {'a','b','c','d'};
+        String str = "abcd";
+        String str2 = new String("xyz");
+
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.next();
+        System.out.println(name);
+    }
+}
