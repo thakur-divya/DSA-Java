f5bc69dcaebb519aed57d3699cd58f9afebeee89
Divya Thakur <thakur.14divya@gmail.com>
2026-02-21 23:28:56 +0530

    learning String Builder

--- Strings/largest.java
+++ Strings/largest.java
@@ -1,5 +1,5 @@
 package Strings;
-
+//time complexity is O(x * n) where x is no of chars in largest string and n is no of strings
 public class largest {
     public static void main(String[] args) {
         String fruits[] = {"mango", "apple", "banana"};
---
+++ Strings/stringbuilder.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class stringbuilder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(" ");
+        for(char ch='a'; ch <='z'; ch++){
+            sb.append(ch);
+        }
+        System.out.println(sb);
+    }
+}
