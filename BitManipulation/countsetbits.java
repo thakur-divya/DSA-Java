2b6b6f46e57758403d4c0912643b26112e4f51bb
Divya Thakur <thakur.14divya@gmail.com>
2026-02-23 19:19:17 +0530

    counting 1 in n

---
+++ BitManipulation/countsetbits.java
@@ -0,0 +1,17 @@
+package BitManipulation;
+
+public class countsetbits {
+    public static int count(int n){
+        int count = 0;
+        while(n > 0){
+            if((n & 1)  != 0){
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println(count(10));
+    }
+}
