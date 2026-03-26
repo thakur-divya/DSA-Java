ad21d4e087a188642c486a3df4467abea30e29a9
Divya Thakur <thakur.14divya@gmail.com>
2026-03-26 11:32:56 +0530

    Optimized Bubble Sort

--- DivideAndConquer/BubbleSort.java
+++ DivideAndConquer/BubbleSort.java
@@ -1,5 +1,6 @@
 package DivideAndConquer;
- //Optimized Bubble Sort
+ //Optimized Bubble Sort 
+ //Best case time comp can be O(n) and worst case is O(n2) if optimized using boolean swapped variable
 public class BubbleSort {
     public static void printArr(int arr[]){
         for(int i=0;i<arr.length-1;i++){
@@ -10,18 +11,25 @@ public class BubbleSort {
     public static void bubble(int arr[]){
         int n = arr.length;
         for(int i=0;i<n-1;i++){
+            boolean swapped = false;
             for(int j=0;j<n-1-i;j++){
                 //swap
                 if(arr[j]>arr[j+1]){
+                    swapped = true;
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
+                
             }
+            if(swapped == false){
+                    System.out.println("Already Sorted" + " ");
+                    return;
+                }
         }
     }
     public static void main(String[] args) {
-        int arr[] = {9,3,5,1,6,0,2,7};
+        int arr[] = {1,2,3,4,5,6};
         bubble(arr);
         printArr(arr);
     }
