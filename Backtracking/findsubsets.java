63073fad41499efb0d2a057c9e2d456547412ead
Divya Thakur <thakur.14divya@gmail.com>
2026-03-28 11:19:30 +0530

    Finding subsets of string

---
+++ Backtracking/findsubsets.java
@@ -0,0 +1,26 @@
+package Backtracking;
+
+public class findsubsets {
+    public static void findsubs(String str, String ans,int i){
+        //base case
+        if(i == str.length()){
+            if(str.length() == 0){
+                System.out.println("null");
+            }
+            else{
+                System.out.println(str);
+            }
+            return;
+        }
+            //recursion
+            //if choice is yes
+            findsubs(str, ans+str.charAt(i),i+1);
+            //if choice is no
+            findsubs(str,ans,i+1);
+        }
+    
+    public static void main(String[] args) {
+        String str = "abc";
+        findsubs(str,"",0);
+    }
+}
