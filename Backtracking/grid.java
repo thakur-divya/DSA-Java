1ece3fc3fa9fbfa0d36023d77bd521b302025572
Divya Thakur <thakur.14divya@gmail.com>
2026-03-29 11:09:27 +0530

    Gridways to reach from source to dest in grid

--- Backtracking/grid.java
+++ Backtracking/grid.java
@@ -1,12 +1,14 @@
 package Backtracking;
-
+//Find the no of ways to reach from (0,0) to (n-1,m-1) in nxm grid. only allowed move is right and down
+//Time complexity is O(2^n+m) not optimised slow run good for small data
 public class grid {
+    // use formula (n-1m-1)!/(n-1)!*(m-1)! to convert into linear time O(n+m)
     public static int gridways(int i,int j,int n,int m){
         //base case
         if(i == n-1 && j==m-1){
             return 1;
         }
-        else if(i == n && j==m){ //handling corner case
+        else if(i >=n || j>=m){ //handling corner case
             return 0;
         }
         //kaam
