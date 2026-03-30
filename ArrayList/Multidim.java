a5378344f6a66fedbf725f1a5ee22db7103e650e
Divya Thakur <thakur.14divya@gmail.com>
2026-03-30 07:31:51 +0530

    Traversing and creating multidimensional ArrayList

--- ArrayList/Multidim.java
+++ ArrayList/Multidim.java
@@ -1,5 +1,29 @@
 package ArrayList;
 import java.util.*;
 public class Multidim {
-    
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> mainlist = new ArrayList<>();
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(5);
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(3);
+        list2.add(4);
+
+        mainlist.add(list1);
+        mainlist.add(list2);
+        System.out.println(mainlist);
+
+        for(int i=0;i<mainlist.size();i++){
+            ArrayList<Integer> currlist = mainlist.get(i);
+            for(int j=0;j<currlist.size();j++){
+                System.out.print(currlist.get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
 }
