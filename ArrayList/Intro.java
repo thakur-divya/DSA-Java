5b21e10eb33b23285b811441aebd4742f7818699
Divya Thakur <thakur.14divya@gmail.com>
2026-03-30 06:23:28 +0530

    Operations on ArrayList

---
+++ ArrayList/Intro.java
@@ -0,0 +1,36 @@
+package ArrayList;
+import java.util.ArrayList;
+public class Intro {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        //Adding an elemenet O(1)
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        //Takes O(n)
+        list.add(0,9); //giving index and element
+
+        System.out.println(list);
+
+        //Getting an element O(1)
+        int element = list.get(2);
+        System.out.println(element); 
+        System.out.println(list.get(4));  
+        
+        //Removing an element - O(n)
+        list.remove(2); //Pass the index of element from list u want to remove
+        System.out.println(list);
+
+        //Set element at index O(n)
+        list.set(2,10); //give index and element to set
+        System.out.println(list);
+
+        //Contains O(n)
+        System.out.println(list.contains(1));
+        System.out.println(list.contains(11));
+    }
+}
