06494f38f6b0e714de0773c41e0cfb2f3143d7e2
Divya Thakur <thakur.14divya@gmail.com>
2026-04-02 09:17:54 +0530

    Pair Sum 1 Brute force approach

---
+++ ArrayList/PairSum.java
@@ -0,0 +1,26 @@
+package ArrayList;
+import java.util.*;
+public class PairSum {
+    public static boolean pairsum1(ArrayList<Integer> list,int target){
+        for(int i=0;i<list.size();i++){
+            for(int j=i+1;j<list.size();j++){
+                if(i+j == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        
+        int target = 5;
+        System.out.println(pairsum1(list, target));
+    }
+}
