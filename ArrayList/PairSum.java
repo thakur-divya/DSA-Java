da5cabc08549a8ef4673432caaf13f8cd5a182d2
Divya Thakur <thakur.14divya@gmail.com>
2026-04-02 09:30:02 +0530

    Pair Sum using brute and two pointer approach

--- ArrayList/PairSum.java
+++ ArrayList/PairSum.java
@@ -1,6 +1,8 @@
 package ArrayList;
 import java.util.*;
 public class PairSum {
+
+    //Brute Force Taking O(n^2) time
     public static boolean pairsum1(ArrayList<Integer> list,int target){
         for(int i=0;i<list.size();i++){
             for(int j=i+1;j<list.size();j++){
@@ -11,6 +13,8 @@ public class PairSum {
         }
         return false;
     }
+
+    //Two Pointer approach taking O(n) time
         public static boolean pairsum2(ArrayList<Integer> list,int target){
             int lp = 0;
             int rp = list.size()-1;
