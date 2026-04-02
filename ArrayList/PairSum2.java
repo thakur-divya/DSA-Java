5e7d481e109f1fa080c0d11b5e3c7a80dffd3087
Divya Thakur <thakur.14divya@gmail.com>
2026-04-02 10:06:12 +0530

    Pair Sum2 using 2 pointer in Sorted rotated array

--- ArrayList/PairSum2.java
+++ ArrayList/PairSum2.java
@@ -3,34 +3,33 @@ package ArrayList;
 import java.util.ArrayList;
 public class PairSum2 {
     public static boolean Sum(ArrayList<Integer> list, int target){
-        int lp,rp;
+    
         int n = list.size();
+        int bp = -1;
 
         for(int i=0;i<n;i++){
-            if(list.get(i) < list.get(i+1)){
-                continue;
+            if(list.get(i) > list.get(i+1)){
+                bp = i;
+                break;
             }
+        }
+
+        int lp = bp+1;
+        int rp = bp;
+        //Modular arithmetic is applied which is used in Advance DSA Concepts
+        while(lp != rp){
+            //case 1
+            if(list.get(lp)+list.get(rp) == target){
+                return true;
+            }
+            //case 2
+            if(list.get(lp)+list.get(rp) < target){
+                lp = (lp+1)%n;
+            }
+            //case 3
             else{
-                rp = i;
-                lp =i+1;
-
-                 //Modular arithmetic is applied which is used in Advance DSA Concepts
-                while(lp != rp){
-                    //case 1
-                    if(list.get(lp)+list.get(rp) == target){
-                        return true;
-                    }
-                    //case 2
-                    else if(list.get(lp)+list.get(rp) < target){
-                        lp = (lp+1)%n;
-                    }
-                    //case 3
-                    else{
-                        rp = (n+rp-1)%n;
-                    }
-                }
+                rp = (n+rp-1)%n;
             }
-
         }
         return false;
     }
