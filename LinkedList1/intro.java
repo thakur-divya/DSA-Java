b25975204b66193b141290720ef035ec770ffd51
Divya Thakur <thakur.14divya@gmail.com>
2026-05-13 19:53:36 +0530

    Getting size of ll

--- LinkedList1/intro.java
+++ LinkedList1/intro.java
@@ -11,6 +11,7 @@ public class intro{
 
     public static Node head;
     public static Node tail;
+    public static int size;
 
     //Methods to perform diff operations
     public void print(){
@@ -24,6 +25,7 @@ public class intro{
     
     public void addFirst(int data){
         Node newNode = new Node(data); //step1 is to create a new node
+        size++;
         if(head == null){
             head = tail = newNode;
             return;
@@ -34,6 +36,7 @@ public class intro{
 
     public void addLast(int data){
         Node newNode = new Node(data);
+        size++;
         if(head == null){
             head = tail = newNode;
             return;
@@ -44,7 +47,12 @@ public class intro{
     }
 
     public void addMiddle(int index, int data){
+        if(index == 0){
+            addFirst(data);
+            return;
+        }
         Node newNode = new Node(data);
+        size++;
         Node temp = head;
         int i = 0;
         while(i < (index - 1)){
@@ -66,5 +74,6 @@ public class intro{
         ll.addLast(4);
         ll.addLast(5);
         ll.print();
+        System.out.println(ll.size);
     }
 }
