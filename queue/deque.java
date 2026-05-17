e32be161e9356af24cd4d8b6f96ce41470e7be6c
Divya Thakur <thakur.14divya@gmail.com>
2026-05-17 22:31:19 +0530

    Reversal of Queue

---
+++ queue/deque.java
@@ -0,0 +1,11 @@
+package queue;
+
+import java.util.*;
+
+public class deque {
+    public static void main(String[] args) {
+        Deque<Integer> d = new LinkedList<>();
+        d.addFirst(1);
+        d.addFirst(2);
+    }
+}
