c6c50d1fe2de7489d8f3278a98d2f765d65963c1
Divya Thakur <thakur.14divya@gmail.com>
2026-05-17 23:01:38 +0530

    Implementing Queue using Deque

--- queue/stackdeque.java
+++ queue/stackdeque.java
@@ -16,6 +16,21 @@ public class stackdeque {
             return d.getLast();
         }
     }
+    //Implementing Queue using Deque
+    static class Queue{
+        Deque<Integer> d = new LinkedList<>(); 
+        
+        public void add(int data){
+            d.addLast(data);
+        }
+        public int remove(){
+            return d.removeFirst();
+        }
+        public int peek(){
+            return d.getFirst();
+        }
+
+    }
     public static void main(String[] args) {
         Stack s = new Stack();
         s.push(1);
@@ -25,5 +40,14 @@ public class stackdeque {
         System.out.println(s.pop());
         System.out.println(s.pop());
         System.out.println(s.pop());
+        System.out.println(" ");
+
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println(q.peek());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
     }
 }
