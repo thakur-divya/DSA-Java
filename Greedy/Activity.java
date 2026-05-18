e5435ab26dab4ba0cb107a04684eebce1b095f41
Divya Thakur <thakur.14divya@gmail.com>
2026-05-18 17:48:54 +0530

    Fractional Knapsack Greedy Problem

--- Greedy/Activity.java
+++ Greedy/Activity.java
@@ -5,7 +5,7 @@ public class Activity {
         int start[] = {1,3,0,5,8,5};
         int end[] ={2,4,6,7,9,9};
 
-        //Sorting
+        //Sorting If we are doing than O(nlogn)
         int activities[][] = new int[start.length][3];
         for(int i=0;i<start.length;i++){
             activities[i][0] = i;
---
+++ Greedy/FracKnapsack.java
@@ -0,0 +1,35 @@
+package Greedy;
+import java.util.*;
+public class FracKnapsack {
+    //Classical Fractional Knapsack Problem using Greedy Approach
+    public static void main(String[] args) {
+        int val[] = {60,100,120};
+        int weight[] = {10,20,30};
+        int W = 50;
+
+        double ratio[][] = new double[val.length][2];
+
+        for(int i=0;i<val.length;i++){
+            ratio[i][0] = i;
+            ratio[i][1] = val[i]/(double)weight[i];
+        }
+        //Sorting in ascending order
+        Arrays.sort(ratio,Comparator.comparingDouble(o -> o[1]));
+
+        int finalVal = 0;
+        int capacity = W;
+        for(int i=ratio.length-1;i>=0;i--){
+            int idx = (int)ratio[i][0];
+            if(capacity >= weight[idx]){
+                capacity -= weight[idx];
+                finalVal += val[idx];
+            }else{
+                finalVal += (ratio[i][1] * capacity);
+                capacity = 0;
+                break;
+            }
+        }
+
+        System.out.println("maximum value = " + finalVal);
+    }
+}
