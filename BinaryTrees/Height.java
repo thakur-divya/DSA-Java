a37b2cac4fb0f2fa8aa27ffbeba9c46227f420e6
Divya Thakur <thakur.14divya@gmail.com>
2026-05-20 21:03:25 +0530

    Height & count of nodes

--- BinaryTrees/Height.java
+++ BinaryTrees/Height.java
@@ -19,6 +19,15 @@ public class Height {
         int rh = height(root.right);
         return Math.max(lh,rh) + 1;
     }
+
+    public static int count(Node root){
+        if(root == null){
+            return 0;
+        }
+        int lc = count(root.left);
+        int rc = count(root.right);
+        return lc+rc+1;
+    }
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
@@ -28,6 +37,7 @@ public class Height {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        System.out.println(height(root));
+        System.out.println("Height of tree is " + height(root));
+        System.out.println("Count of Nodes is " + count(root));
     }
 }
