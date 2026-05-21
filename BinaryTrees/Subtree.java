e1bb43d1fc1fecc6242ee3785f4e26ee69276cc7
Divya Thakur <thakur.14divya@gmail.com>
2026-05-21 13:50:49 +0530

    Checking if subtree exists

---
+++ BinaryTrees/Subtree.java
@@ -0,0 +1,59 @@
+package BinaryTrees;
+import  java.util.*;
+public class Subtree {
+    static class Node{
+        int data;
+        Node left,right;
+
+        public Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static boolean isIdentical(Node node,Node subroot){
+        if(node == null && subroot == null){
+            return true;
+        }
+        else if(node == null || subroot == null || node.data != subroot.data){
+            return false;
+        }
+
+        if(!isIdentical(node.left, subroot.left)){
+            return false;
+        }
+        if(!isIdentical(node.right, subroot.right)){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean issubTree(Node root,Node subroot){
+        if(root == null){
+            return false;
+        }
+
+        if(root.data == subroot.data){
+            if(isIdentical(root,subroot)){
+                return true;
+            }
+        }
+
+        return issubTree(root.left, subroot) || issubTree(root.right,subroot);
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.right = new Node(6);
+
+        Node subroot = new Node(2);
+        subroot.left = new Node(4);
+        subroot.right = new Node(5);
+
+        System.out.println(issubTree(root, subroot));
+    }
+}
