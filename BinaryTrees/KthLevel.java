9bac5cdb0ef150abcf4f7961723f9dffd9bc5533
Divya Thakur <thakur.14divya@gmail.com>
2026-05-23 00:09:40 +0530

    Kth Level Of Tree

---
+++ BinaryTrees/KthLevel.java
@@ -0,0 +1,44 @@
+package BinaryTrees;
+public class KthLevel {
+    //Can be solved using both recursion and iteration
+    static class Node{
+        int data;
+        Node left,right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+            
+        }
+    }
+
+    public static void Klevel(Node root, int level,int k){
+        //Base case
+        if(root == null){
+            return;
+        }
+
+        if(level == k){
+            System.out.print(root.data + " ");
+            return;
+        }
+
+        Klevel(root.left, level+1, k);
+        Klevel(root.right, level+1, k);
+    }
+    public static void main(String[] args) {
+        
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int k = 3;
+
+        Klevel(root,1,k);
+    }
+}
