361fa332725310e4cb122011c5d48821a4bcb6b1
Divya Thakur <thakur.14divya@gmail.com>
2026-05-25 11:12:20 +0530

    Second Approach

--- BinaryTrees/LowestCommonAncestor.java
+++ BinaryTrees/LowestCommonAncestor.java
@@ -56,7 +56,30 @@ public class LowestCommonAncestor {
     }
 
     //Approach 2
-    
+
+    //No Auxilary Data Structure like ArrayList is used in this approach O(N)
+    public static Node lca2(Node root,int n1,int n2){
+
+        if(root == null){
+            return null;
+        }
+
+        if(root.data == n1 || root.data == n2){
+            return root;
+        }
+
+        Node leftLca = lca2(root.left, n1,n2);
+        Node rightLca = lca2(root.right ,n1,n2);
+
+        if(rightLca == null){
+            return leftLca;
+        }
+        if(leftLca == null){
+            return rightLca;
+        }
+
+        return root;
+    }
     public static void main(String[] args) {
         
         Node root = new Node(1);
@@ -67,9 +90,10 @@ public class LowestCommonAncestor {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        int n1 = 3;
-        int n2 = 7;
+        int n1 = 4;
+        int n2 = 6;
 
         System.out.println(lca(root,n1,n2).data);
+        System.out.println(lca2(root,n1,n2).data);
     }
 }
