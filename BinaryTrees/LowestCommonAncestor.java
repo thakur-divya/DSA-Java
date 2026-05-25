cc229aeee117a36396f5a3f35c802030c1bd71cf
Divya Thakur <thakur.14divya@gmail.com>
2026-05-25 12:02:02 +0530

    Min dist bw 2 nodes

--- BinaryTrees/LowestCommonAncestor.java
+++ BinaryTrees/LowestCommonAncestor.java
@@ -80,6 +80,35 @@ public class LowestCommonAncestor {
 
         return root;
     }
+
+
+    //Program to find min distance between 2 nodes
+    public static int lcaDist(Node root,int n){
+        if(root == null){
+            return -1;
+        }
+        if(root.data == n){
+            return 0;
+        }
+
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+
+        if(leftDist == -1 && rightDist == -1){
+            return -1;
+        }else if(leftDist == -1){
+            return rightDist+1;
+        }else{
+            return leftDist+1;
+        }
+    }
+    public static int mindist(Node root,int n1,int n2){
+        Node lca = lca2(root,n1,n2);
+        int dist1 = lcaDist(lca,n1);
+        int dist2 = lcaDist(lca,n2);
+
+        return dist1 + dist2;
+    }
     public static void main(String[] args) {
         
         Node root = new Node(1);
@@ -93,7 +122,9 @@ public class LowestCommonAncestor {
         int n1 = 4;
         int n2 = 6;
 
-        System.out.println(lca(root,n1,n2).data);
-        System.out.println(lca2(root,n1,n2).data);
+        System.out.println(lca(root,n1,n2).data); //lca app 1
+        System.out.println(lca2(root,n1,n2).data); //lca app 2
+
+        System.out.println(mindist(root, n1, n2));
     }
 }
