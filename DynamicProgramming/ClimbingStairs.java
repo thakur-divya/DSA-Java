eb482aa83cf5989f3d267bbc978acb4cec09c9cc
Divya Thakur <thakur.14divya@gmail.com>
2026-05-27 22:00:47 +0530

    Using tabulation

--- DynamicProgramming/ClimbingStairs.java
+++ DynamicProgramming/ClimbingStairs.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 //Count ways to reach nth stair. one can climb 1 or 2 stairs at a time
 public class ClimbingStairs {
-    public static int countWays(int n,int ways[]){  //Using only recursion + memoization
+    public static int countWays(int n,int ways[]){  //Using only recursion(2^n) + memoization O(n)
         if(n == 0){
             return 1;
         }
@@ -17,10 +17,23 @@ public class ClimbingStairs {
         ways[n] = countWays(n-1,ways) + countWays(n-2,ways);
         return ways[n];
     }
+
+    public static int countab(int n){
+        int dp[] = new int[n+1]; // 0 to n
+        dp[0] = 1;
+        for(int i=1;i<=n;i++){
+            if(i == 1){
+                dp[i] = dp[i-1] + 0;
+            }else{
+            dp[i] = dp[i-1] + dp[i-2];
+        }}
+        return dp[n];
+    }
     public static void main(String[] args) {
         int n = 5;
         int ways[] = new int[n+1]; // 0 0 0 0 
         Arrays.fill(ways, -1); //-1 -1 -1 -1
         System.out.println(countWays(n,ways));
+        System.out.println(countab(n));
     }
 }
