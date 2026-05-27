08cd4c14c55c67c624f1d28cb63fb73cd72aa851
Divya Thakur <thakur.14divya@gmail.com>
2026-05-27 13:19:38 +0530

    dp tabulation

--- DynamicProgramming/fib.java
+++ DynamicProgramming/fib.java
@@ -11,10 +11,21 @@ public class fib {
         f[n] = fibonacci(n-1,f)+fibonacci(n-2,f);
         return f[n];
     }
+
+    public static int fibtabulation(int n){
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
     public static void main(String[] args) {
         int n = 5;
         int f[] = new int[n+1];
         System.out.println(fibonacci(n,f));
+        System.out.println(fibtabulation(n));
     }
     
 }
