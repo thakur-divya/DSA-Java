1608d0c18775b764072bb1b040db95cbb74463a0
Divya Thakur <thakur.14divya@gmail.com>
2026-05-27 17:29:26 +0530

    Using Memoization

--- DynamicProgramming/ClimbingStairs.java
+++ DynamicProgramming/ClimbingStairs.java
@@ -1,18 +1,26 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
+
 //Count ways to reach nth stair. one can climb 1 or 2 stairs at a time
 public class ClimbingStairs {
-    public static int countWays(int n){  //Using only recursion
+    public static int countWays(int n,int ways[]){  //Using only recursion + memoization
         if(n == 0){
             return 1;
         }
         if(n < 0){
             return 0;
         }
-        return countWays(n-1) + countWays(n-2);
+        if(ways[n] != -1){
+            return ways[n];
+        }
+        ways[n] = countWays(n-1,ways) + countWays(n-2,ways);
+        return ways[n];
     }
     public static void main(String[] args) {
         int n = 5;
-        System.out.println(countWays(n));
+        int ways[] = new int[n+1]; // 0 0 0 0 
+        Arrays.fill(ways, -1); //-1 -1 -1 -1
+        System.out.println(countWays(n,ways));
     }
 }
