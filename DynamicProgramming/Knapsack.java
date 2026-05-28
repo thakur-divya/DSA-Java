9c1f6e593d489cd980107a394eca2609d29c3264
Divya Thakur <thakur.14divya@gmail.com>
2026-05-28 14:39:32 +0530

    Using Memoization Technique

--- DynamicProgramming/Knapsack.java
+++ DynamicProgramming/Knapsack.java
@@ -1,21 +1,26 @@
 package DynamicProgramming;
 public class Knapsack {
-    
-    public static int knap(int val[],int wt[],int W,int i){
+
+    public static int knap(int val[],int wt[],int W,int n,int dp[][]){
         
-        if(W == 0 || i == 0){
+        if(W == 0 || n == 0){
             return 0;
         }
 
-        if(wt[i] <= W){ //valid
+        if(dp[n][W] != -1){
+            return dp[n][W];
+        }
+        if(wt[n-1] <= W){ //valid
             //include
-            int ans1 = val[i] + knap(val, wt, W-wt[i], i-1);
+            int ans1 = val[n-1] + knap(val, wt, W-wt[n-1], n-1,dp);
             //exclude
-            int ans2 = knap(val, wt, W, i-1);
-            return Math.max(ans1, ans2);
+            int ans2 = knap(val, wt, W, n-1,dp);
+            dp[n][W] =  Math.max(ans1, ans2);
+            return dp[n][W];
         }else{
             //Not Valid
-            return knap(val, wt, W, i-1);
+            dp[n][W] = knap(val, wt, W, n-1,dp);
+            return dp[n][W];
         }
         
     }
@@ -23,7 +28,12 @@ public class Knapsack {
     int val[] = {15,14,10,45,30};
     int wt[] = {2,5,1,3,4};
     int W = 7;
-    int idx = val.length-1;
-    System.out.println(knap(val, wt, W, idx));
+    int dp[][] = new int[val.length+1][W+1];
+    for(int i=0;i<dp.length;i++){
+        for(int j=0;j<dp[0].length;j++){
+            dp[i][j] = -1;
+        }
+    }
+    System.out.println(knap(val, wt, W,val.length,dp));
     }
 }
