e9e892cbb749bb95d4bc180a344bf4a524441389
Divya Thakur <thakur.14divya@gmail.com>
2026-05-30 13:03:48 +0530

    Knapsack using tabulation

--- DynamicProgramming/Knapsack.java
+++ DynamicProgramming/Knapsack.java
@@ -30,6 +30,38 @@ public class Knapsack {
     //initaially i[0]j[0] = 0
     //Start loop from 1 to n+1
     //dp[n+1][W+1] from 0 to n+1
+
+    public static int knaptab(int val[],int wt[],int W){
+        int n = val.length;
+        int dp[][] = new int[n+1][W+1];
+        for(int i=0;i<dp.length;i++){  //initialising all rows in 0th Col as 0
+            dp[i][0] = 0;  
+        }
+        for(int j=0;j<dp[0].length;j++){ //initialising all cols in 0th Row as 0
+            dp[0][j] = 0;
+        }
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<W+1;j++){
+                int v = val[i-1];  //val of ith item
+                int w = wt[i-1];   //weight of ith item
+                if(w <= j){        //valid condition
+                    //include
+                    int incprof = v + dp[i-1][j-w];
+                    //exclude
+                    int excprof = dp[i-1][j];
+                    dp[i][j] = Math.max(incprof,excprof);
+                  
+                }else{
+                    //invalid condition
+                    int excprof = dp[i-1][j];
+                    dp[i][j] = excprof;
+                   
+                }
+            }
+        }
+        return dp[n][W];
+    }
     public static void main(String[] args) {
     int val[] = {15,14,10,45,30};
     int wt[] = {2,5,1,3,4};
@@ -41,5 +73,6 @@ public class Knapsack {
         }
     }
     System.out.println(knap(val, wt, W,val.length,dp));
+    System.out.println(knaptab(val, wt, W));
     }
 }
