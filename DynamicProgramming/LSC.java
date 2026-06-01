e646fc4fc9baf39e90caf72303f68ee1713ecff0
Divya Thakur <thakur.14divya@gmail.com>
2026-06-01 17:59:49 +0530

    Using Tabulation

--- DynamicProgramming/LSC.java
+++ DynamicProgramming/LSC.java
@@ -13,13 +13,66 @@ public class LSC {
             return Math.max(ans1,ans2);
         }
     }
+    /*Qki recursion k andar Stack banta h aur bahot cases me stack overflow ho jata h
+    isliye go for tabulation */
+    //Using recursion + memoization O(n*m)
+    public static int lcs2(String str1,String str2,int n,int m,int dp[][]){   
+        if(n == 0 || m == 0){
+            return 0;
+        }
+        
+        if(dp[n][m] != -1){
+            return dp[n][m];
+        }
+        if(str1.charAt(n-1) == str2.charAt(m-1)){
+            dp[n][m] = lcs(str1, str2, n-1, m-1) + 1;
+            return dp[n][m];
+        }else{
+            int ans1 = lcs2(str1, str2, n-1, m,dp);
+            int ans2 = lcs2(str1, str2, n, m-1,dp);
+            dp[n][m] = Math.max(ans1,ans2);
+            return dp[n][m];
+        }
+    }
+
+    //Using Tabulation
+    public static int lcs3(String str1,String str2){
+        int n = str1.length();
+        int m = str2.length();
+
+        int dp[][] = new int[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                dp[i][j] = 0;
+            }
+        }
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(str1.charAt(i-1) == str2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    int ans1 = dp[i-1][j];
+                    int ans2 = dp[i][j-1];
+                    dp[i][j] = Math.max(ans1,ans2);
+                }
+            }
+        }
+        return dp[n][m];
+    }
     public static void main(String[] args) {
         String str1 = "abcdge";
         String str2 = "abedg";
         int n = str1.length();
         int m = str2.length();
-        System.out.println(lcs(str1, str2, str1.length(),str2.length()));
-
-        
+        int dp[][] = new int[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                dp[i][j]=-1;
+            }
+        }
+        System.out.println(lcs(str1, str2, n,m));
+        System.out.println(lcs2(str1, str2, n,m,dp));
+        System.out.println(lcs3(str1, str2));
     }
 }
