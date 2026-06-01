6858bc79bedc23a74efe41c9083d86cce0cc66e2
Divya Thakur <thakur.14divya@gmail.com>
2026-06-01 13:55:08 +0530

    Unbounded

--- DynamicProgramming/UnboundedKnapsack.java
+++ DynamicProgramming/UnboundedKnapsack.java
@@ -1,6 +1,15 @@
 package DynamicProgramming;
 
 public class UnboundedKnapsack {
+    public static void print(int dp[][]){
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
     public static int unbounded(int val[],int wt[],int W){
         int n = val.length;
         int dp[][] = new int[n+1][W+1];
@@ -21,7 +30,7 @@ public class UnboundedKnapsack {
                 }
             }
         }
-        //print(dp);
+        print(dp);
         return dp[n][W];
     }
     public static void main(String[] args) {
