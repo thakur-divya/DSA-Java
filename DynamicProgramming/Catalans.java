8359af0a04d8521a28e04548ff9d580b1c7b75a4
Divya Thakur <thakur.14divya@gmail.com>
2026-06-02 18:22:32 +0530

    Using Tabulation

--- DynamicProgramming/Catalans.java
+++ DynamicProgramming/Catalans.java
@@ -1,5 +1,5 @@
 package DynamicProgramming;
-
+import java.util.*;
 public class Catalans {               
 
     public static int Cat(int n){   //Using Recursion
@@ -12,8 +12,42 @@ public class Catalans {
         }
         return ans;
     }
+
+    public static int CatMemo(int n,int dp[]){      //Using Memoization
+        //base case
+        if(n==0 || n==1){
+            return 1;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        int ans = 0;
+        for(int i=0;i<=n-1;i++){
+            ans += CatMemo(i, dp) * CatMemo(n-i-1, dp);
+        }
+        return ans;
+    }
+
+    public static int Cattab(int n){ //Using Tabulation O(n*n)
+        int dp[] = new int[n+1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2;i<=n;i++){
+            for(int j=0;j<=i-1;j++){
+                dp[i] += dp[j] * dp[i-j-1];
+            }
+        }
+        
+        return dp[n];
+    }
     public static void main(String[] args) {
         int n = 5;
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
         System.out.println(Cat(n));
+        System.out.println(CatMemo(4, dp));
+        System.out.println(Cattab(3));
     }
 }
