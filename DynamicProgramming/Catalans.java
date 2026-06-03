bbc3acd4dba004cfbb456f4fdfe063bc385ec21a
Divya Thakur <thakur.14divya@gmail.com>
2026-06-03 15:13:07 +0530

    Count BST

--- DynamicProgramming/Catalans.java
+++ DynamicProgramming/Catalans.java
@@ -42,6 +42,21 @@ public class Catalans {
         
         return dp[n];
     }
+
+    public static int countbsts(int n){   //Count BSTs
+         int dp[] = new int[n+1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2;i<=n;i++){
+            for(int j=0;j<=i-1;j++){
+                dp[i] += dp[j] * dp[i-j-1];
+            }
+        }
+        
+        return dp[n];
+    }
     public static void main(String[] args) {
         int n = 5;
         int dp[] = new int[n+1];
