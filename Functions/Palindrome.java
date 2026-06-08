863518651ff6383f29f937ede6fabd777b52259e
Divya Thakur <thakur.14divya@gmail.com>
2026-06-08 15:38:50 +0530

    Valid Palindrome Leetcode

--- Functions/Palindrome.java
+++ Functions/Palindrome.java
@@ -2,6 +2,31 @@ package Functions;
 import java.util.Scanner;
 
 public class Palindrome {
+
+    //Valid Palindrome Leetcode q 125 using 2 pointers
+    public static boolean valid(String s){
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right){
+
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;     //Skip any extra chars except alphanumeric
+            }
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+            
+        }
+        return true;
+    }
+
     public static void main(String args[]){
         int n,temp,rev;
         Scanner sc = new Scanner(System.in);
@@ -15,6 +40,8 @@ public class Palindrome {
         else{
             System.out.println(n + "is not palindrome");
         }
+
+        System.out.println(valid("A man, a plan, a canal: Panama"));
 
     }
     public static int palindrome(int n){
