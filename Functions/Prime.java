4d62d1d50d8fc75ce0539e091a16bc87668fc523
Divya Thakur <thakur.14divya@gmail.com>
2026-06-08 16:04:25 +0530

    Simple Approch for primes in range

--- Functions/Prime.java
+++ Functions/Prime.java
@@ -30,7 +30,23 @@ class Prime{
         }
     }
 
-    
+    //Simple Approach
+    public static int simple(int n){
+        int count = 0;
+        for(int i=2;i<=n;i++){
+            boolean prime = true;
+            for(int j=2;j*j<=i;j++){
+                if(i%j == 0){
+                    prime = false;
+                    break;
+                }
+            }
+            if(prime){
+                count++;
+            }
+        }
+        return count;
+    }
     //Sieve of Eratosthenes Primes in Range leetcode q. 
     public static int isPriminRange(int n){
         int count = 0;
@@ -65,5 +81,6 @@ class Prime{
         System.out.println("");
         */
         System.out.println(isPriminRange(10));
+        System.out.println(simple(10));
     }
 }
