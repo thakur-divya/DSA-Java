f3b21b4c57c69cf9fe176969c29144a4d14d27e7
Divya Thakur <thakur.14divya@gmail.com>
2026-06-08 15:57:35 +0530

    Primes in Range Leetcode

--- Functions/Prime.java
+++ Functions/Prime.java
@@ -30,13 +30,40 @@ class Prime{
         }
     }
 
+    
+    //Sieve of Eratosthenes Primes in Range leetcode q. 
+    public static int isPriminRange(int n){
+        int count = 0;
+        boolean isPrime[] = new boolean[n+1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for(int i=2;i*i<=n;i++){
+           
+            if(isPrime[i]){
+                for(int j=i*i ; j <= n; j+=i){
+                    isPrime[j] = false;
+                }
+           }
+        }
+
+        for(int i=0;i<isPrime.length;i++){
+            if(isPrime[i]){
+                count++;
+            }
+        }
+        return count;
+    }
     public static void main(String args[]){
         int n;
-        Scanner sc = new Scanner(System.in);
+        /*Scanner sc = new Scanner(System.in);
         System.out.println("Enter n : ");
         n=sc.nextInt();
         Prime ob = new Prime();
         ob.inRange(n);
-
+        System.out.println("");
+        */
+        System.out.println(isPriminRange(10));
     }
 }
