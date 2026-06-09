dc35053c299cd0a9a489c2a1c3dfc1a2f758c294
Divya Thakur <thakur.14divya@gmail.com>
2026-06-09 13:37:00 +0530

    Pattern

--- Pattern/InvertedPatternwithnos.java
+++ Pattern/InvertedPatternwithnos.java
@@ -3,10 +3,10 @@ package Pattern;
 public class InvertedPatternwithnos {
     public static void InvertedPatternwithNos(int n){
         for(int i=n;i>=1;i--){
-            int k=1;
+            //int k=1;
             for(int j=1;j<=i;j++){
-                System.out.print(k);
-                k++;
+                System.out.print(j+ " ");
+                //k++;
             }
             System.out.println();
         }
