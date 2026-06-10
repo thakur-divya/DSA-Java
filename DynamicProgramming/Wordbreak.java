0e61136c47db7e3a3bce79f7003a77d95e444bb0
Divya Thakur <thakur.14divya@gmail.com>
2026-06-10 23:54:11 +0530

    Word breaks

--- DynamicProgramming/Wordbreak.java
+++ DynamicProgramming/Wordbreak.java
@@ -3,7 +3,12 @@ package DynamicProgramming;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+/*
+Given a string s and a dictionary of strings wordDict, 
+return true if s can be segmented into a space-separated
+sequence of one or more dictionary words.
 
+Note that the same word in the dictionary may be reused multiple times in the segmentation. */
 public class Wordbreak {
     public static boolean wordBreak(String s, List<String> wordDict) {
         HashSet<String> set = new HashSet<>(wordDict);
