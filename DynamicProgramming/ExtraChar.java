348f6d3a4e27b7c16049726b0dc3ab77da20ae34
Divya Thakur <thakur.14divya@gmail.com>
2026-06-10 23:44:47 +0530

    Extra char min

--- DynamicProgramming/ExtraChar.java
+++ DynamicProgramming/ExtraChar.java
@@ -1,7 +1,11 @@
 package DynamicProgramming;
 
 import java.util.HashSet;
-
+/*
+You are given a 0-indexed string s and a dictionary of words dictionary. 
+You have to break s into one or more non-overlapping substrings such that 
+each substring is present in dictionary.
+ There may be some extra characters in s which are not present in any of the substrings. */
 public class ExtraChar {
     //Return the minimum number of extra characters left over if you break up s optimally.
     public static int minExtraChar(String s, String[] dictionary) {
