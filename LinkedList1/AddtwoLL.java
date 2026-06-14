6814488928748a9b2fc6ba862625475faf9dba0a
Divya Thakur <thakur.14divya@gmail.com>
2026-06-14 23:00:26 +0530

    Add two LL

--- LinkedList1/AddtwoLL.java
+++ LinkedList1/AddtwoLL.java
@@ -1,38 +1,62 @@
 package LinkedList1;
 
-import java.util.*;
-public class AddtwoLL.java{
-    
-  //Definition for singly-linked list.
-  public class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
- 
-class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-        int carry = 0;
+public class AddtwoLL {
 
-        while (l1 != null || l2 != null || carry != 0) {
-            int x = (l1 != null) ? l1.val : 0;
-            int y = (l2 != null) ? l2.val : 0;
+    static class ListNode {
+        int val;
+        ListNode next;
 
-            int sum = x + y + carry;
-            carry = sum / 10;
+        ListNode() {}
 
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
-
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
+        ListNode(int val) {
+            this.val = val;
         }
 
-        return dummy.next;
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
-}
+
+    static class Solution {
+        public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode dummy = new ListNode(0);
+            ListNode curr = dummy;
+            int carry = 0;
+
+            while (l1 != null || l2 != null || carry != 0) {
+                int x = (l1 != null) ? l1.val : 0;
+                int y = (l2 != null) ? l2.val : 0;
+
+                int sum = x + y + carry;
+                carry = sum / 10;
+
+                curr.next = new ListNode(sum % 10);
+                curr = curr.next;
+
+                if (l1 != null) l1 = l1.next;
+                if (l2 != null) l2 = l2.next;
+            }
+
+            return dummy.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode ans = Solution.addTwoNumbers(l1, l2);
+
+        while (ans != null) {
+            System.out.print(ans.val + " ");
+            ans = ans.next;
+        }
+    }
 }
