eca57dc806f96d1ececad80777f8c7eeec0bb206
Divya Thakur <thakur.14divya@gmail.com>
2026-06-15 14:43:38 +0530

    Root to leaf path

--- BST/Basics.java
+++ BST/Basics.java
@@ -1,4 +1,5 @@
 package BST;
+import java.util.*;
 //Binary Search Tree
 public class Basics {
     static class Node{
@@ -100,6 +101,31 @@ public class Basics {
                 PrintInRange(root.right, k1, k2);
             }
         }
+
+        //Print Root to Leaf Path
+        public static void printRoot2Leaf(Node root,ArrayList<Integer> path){
+            if(root == null){
+                return;
+            }
+
+            path.add(root.data); //Step 1
+            if(root.left == null && root.right == null){
+                printPath(path);
+            }
+
+            printRoot2Leaf(root.left, path); //Step 2
+            printRoot2Leaf(root.right,path); //Step 3
+            path.remove(path.size()-1);
+
+        }
+
+        public static void printPath(ArrayList<Integer> path){
+            for(int i=0;i<path.size();i++){
+                System.out.print(path.get(i) + "->");
+            }
+            System.out.println("Null");
+        }
+
         public static void main(String[] args) {
             int values[] = {5,1,3,4,2,7};
             Node root = null;
@@ -122,6 +148,9 @@ public class Basics {
 
 
             PrintInRange(root, 2, 8);
+            System.out.println("");
+
+            printRoot2Leaf(root, new ArrayList<>());
         }
 }
 
