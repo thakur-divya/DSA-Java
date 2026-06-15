04663f3ae9bd4d37c9279ca9f6f65ea30c3ec05f
Divya Thakur <thakur.14divya@gmail.com>
2026-06-15 13:57:39 +0530

    Deleting a key

--- BST/Basics.java
+++ BST/Basics.java
@@ -48,6 +48,46 @@ public class Basics {
                 return search(root.right, key);
             }
         }
+
+        //Deleting a Node - V.Imp
+        public static Node delete(Node root,int val){
+            if(root.data < val){
+                root.right = delete(root.right,val);
+            }
+            else if(root.data >  val){
+                root.left = delete(root.left, val);
+            }
+
+            else{ //voila
+
+                //case 1 - Deleting a leaf node - Delete Node and return null to parent
+                if(root.left == null && root.right == null){
+                    return null;
+                }
+                
+                //case 2 - Single Child - Delete Node & replace node with child node
+                if(root.left == null){
+                    return root.right;
+                }
+                else if(root.right ==  null){
+                    return root.left;
+                }
+
+                //Case 3 - Two Children - Replace value with Inorder Successor. Delete the node for IS.
+                Node IS = findInorderSuccessor(root.right);
+                root.data = IS.data;
+                delete(root.right, IS.data);
+            }
+
+            return root;
+        }
+
+        public static Node findInorderSuccessor(Node root){
+            while(root.left != null){
+                root = root.left;
+            }
+            return root;
+        }
         public static void main(String[] args) {
             int values[] = {5,1,3,4,2,7};
             Node root = null;
@@ -63,6 +103,10 @@ public class Basics {
             }else{
                 System.out.println("Not Found");
             }
+
+            root = delete(root,1);
+            System.out.println("");
+            inorder(root);
         }
 }
 
