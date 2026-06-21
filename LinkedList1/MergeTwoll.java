6043005edf09251fd587f54475cc6ab4345640c6
Divya Thakur <thakur.14divya@gmail.com>
2026-06-21 23:51:49 +0530

    Merge two LL

---
+++ LinkedList1/MergeTwoll.java
@@ -0,0 +1,28 @@
+public class MergeTwoll{
+  
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+
+        // Attach remaining nodes
+        if (list1 != null) {
+            temp.next = list1;
+        } else {
+            temp.next = list2;
+        }
+
+        return dummy.next;
+    
+}
+}
