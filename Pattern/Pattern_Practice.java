f180c5741c5318cb83df6d4b167a680ee82f84a5
Divya Thakur <thakur.14divya@gmail.com>
2026-09-05 11:44:58 +0530

    Pattern Practices

--- Pattern/Pattern_Practice.java
+++ Pattern/Pattern_Practice.java
@@ -22,6 +22,16 @@ public class Pattern_Practice {
             }
             System.out.println();
         }
+
+        System.out.println("4th Pattern");
+        char ch = 'A';
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
     }
 
 }
