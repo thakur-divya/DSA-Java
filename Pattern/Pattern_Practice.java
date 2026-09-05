1af3c5f2653b074ecff6ec8b8ad4abe26604240d
Divya Thakur <thakur.14divya@gmail.com>
2026-09-05 11:56:45 +0530

    Pattern Practices

--- Pattern/Pattern_Practice.java
+++ Pattern/Pattern_Practice.java
@@ -32,6 +32,19 @@ public class Pattern_Practice {
             }
             System.out.println();
         }
+
+        System.out.println("5th Pattern (Hollow Pattern)");
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=5;j++){
+
+                if(i==1 || i==4 || j==1 || j==5){
+                System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 
 }
