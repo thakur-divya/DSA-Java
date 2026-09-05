8ab00fc18e4c9a11d2e9fe5530b6a6c58bbbbdce
Divya Thakur <thakur.14divya@gmail.com>
2026-09-05 14:54:40 +0530

    Pattern Practices

--- Pattern/Pattern_Practice.java
+++ Pattern/Pattern_Practice.java
@@ -100,7 +100,7 @@ public class Pattern_Practice {
             System.out.println();
         }
 
-        System.out.println("8th Pattern");
+        System.out.println("8th Pattern ButterFly Pattern");
         int num = 4;
         //1st halves
         for(int i=1;i<=num;i++){
@@ -128,6 +128,34 @@ public class Pattern_Practice {
             }
             for(int j=1;j<=i;j++){
                 System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+        System.out.println("9th Pattern Solid Rhombus Pattern");
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("10th Pattern Hollow Rhombus Pattern");
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=5;j++){
+                if(i==1 || i==5 || j==1 || j==5){
+                System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
