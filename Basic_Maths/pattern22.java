f1e09c72f20de91b6b3d1aab5a951e1bed481567
Divya Thakur <thakur.14divya@gmail.com>
2026-09-07 18:26:15 +0530

    Basics

--- Basic_Maths/pattern22.java
+++ Basic_Maths/pattern22.java
@@ -9,5 +9,76 @@ public class pattern22 {
             }
             System.out.println();
         }
+
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                if(i==1 || i==4 || j==1 || j==4){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+
+
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int k=2*(4-i);k>0;k--){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=3;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int k=1;k<=2*(4-i);k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        for(int i=4;i>=1;i--){
+            //left
+            for(int j=i;j>=1;j--){
+                System.out.print("*");
+            }
+            //spaces
+            for(int k=0;k<2*(4-i);k++){
+                System.out.print(" ");
+            }
+            //right
+            for(int j=i;j>=1;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=4;i++){
+            //left
+            for(int j=i;j>=1;j--){
+                System.out.print("*");
+            }
+            //spaces
+            for(int k=0;k<2*(4-i);k++){
+                System.out.print(" ");
+            }
+            //right
+            for(int j=i;j>=1;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
