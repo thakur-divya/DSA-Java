189c2a2d4b2ba2cbabbe37a3de8bcd8ad4ebea40
Divya Thakur <thakur.14divya@gmail.com>
2026-09-07 16:58:18 +0530

    Basics

--- Basic_Maths/divisor.java
+++ Basic_Maths/divisor.java
@@ -15,9 +15,13 @@ public class divisor {
         }
         return result;*/
         List<Integer> list = new ArrayList<>();
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=Math.sqrt(n);i++){
             if(n%i == 0){
                 list.add(i);
+
+                if (i != n / i) {
+                    list.add(n / i);
+                }
             }
         }
         return list;
---
+++ Basic_Maths/prime.java
@@ -0,0 +1,17 @@
+public class prime {
+    public static boolean isPrime(int n) {
+        if(n == 2){
+            return true;
+        }
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int n = 11;
+        System.out.println(isPrime(n));
+    }
+}
